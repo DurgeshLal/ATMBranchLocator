8877f2ef2cd099a39d089c580bda094f7199da62
DurgeshLal <durgesh.cs.06@live.com>
2015-08-15 18:02:14 -0400

    Made change

--- JCATMLocator/Model/JCATMBranch.m
+++ JCATMLocator/Model/JCATMBranch.m
@@ -32,23 +32,23 @@
     
     self = [super init];
     if (self) {
-        self.access = (dict[ACCESS] ? dict[ACCESS] : [NSNull null]);
-        self.address = (dict[ADDRESS] ? dict[ADDRESS] : [NSNull null]);
-        self.bank = (dict[BANK] ? dict[BANK] : [NSNull null]);
-        self.city = (dict[CITY] ? dict[CITY] : [NSNull null]);
-        self.distance = (dict[DISTANCE] ? dict[DISTANCE] : [NSNull null]);
-        self.label = (dict[LABEL] ? dict[LABEL] : [NSNull null]);
-        self.lat = (dict[LAT] ? dict[LAT] : [NSNull null]);
-        self.lng = (dict[LNG] ? dict[LNG] : [NSNull null]);
-        self.locType = (dict[LOC_TYPE] ? dict[LOC_TYPE] : [NSNull null]);
-        self.name = (dict[NAME] ? dict[NAME] : [NSNull null]);
-        self.state = (dict[STATE] ? dict[STATE] : [NSNull null]);
-        self.zip = (dict[ZIP] ? dict[ZIP] : [NSNull null]);
-        self.phone = (dict[PHONE] ? dict[PHONE] : [NSNull null]);
-        self.languages = (dict[LANGUAGES] ? dict[LANGUAGES] : [NSNull null]);
-        self.services = (dict[SERVICES] ? dict[SERVICES] : [NSNull null]);
-        self.driveUpHrs = (dict[DRIVE_UP_HRS] ? dict[DRIVE_UP_HRS] : [NSNull null]);
-        self.lobbyHrs = (dict[LOBBY_HRS] ? dict[LOBBY_HRS] : [NSNull null]);
+        self.access = (![dict[ACCESS] isKindOfClass:[NSNull class]] ? dict[ACCESS] : [NSNull null]);
+        self.address = (![dict[ADDRESS] isKindOfClass:[NSNull class]] ? dict[ADDRESS] : [NSNull null]);
+        self.bank = (![dict[BANK] isKindOfClass:[NSNull class]] ? dict[BANK] : [NSNull null]);
+        self.city = (![dict[CITY] isKindOfClass:[NSNull class]] ? dict[CITY] : [NSNull null]);
+        self.distance = (![dict[DISTANCE] isKindOfClass:[NSNull class]] ? dict[DISTANCE] : [NSNull null]);
+        self.label = (![dict[LABEL] isKindOfClass:[NSNull class]] ? dict[LABEL] : [NSNull null]);
+        self.lat = (![dict[LAT] isKindOfClass:[NSNull class]] ? dict[LAT] : [NSNull null]);
+        self.lng = (![dict[LNG] isKindOfClass:[NSNull class]] ? dict[LNG] : [NSNull null]);
+        self.locType = (![dict[LOC_TYPE] isKindOfClass:[NSNull class]] ? dict[LOC_TYPE] : [NSNull null]);
+        self.name = (![dict[NAME] isKindOfClass:[NSNull class]] ? dict[NAME] : [NSNull null]);
+        self.state = (![dict[STATE] isKindOfClass:[NSNull class]] ? dict[STATE] : [NSNull null]);
+        self.zip = (![dict[ZIP] isKindOfClass:[NSNull class]] ? dict[ZIP] : [NSNull null]);
+        self.phone = (![dict[PHONE] isKindOfClass:[NSNull class]] ? dict[PHONE] : [NSNull null]);
+        self.languages = (![dict[LANGUAGES] isKindOfClass:[NSNull class]] ? dict[LANGUAGES] : [NSNull null]);
+        self.services = (![dict[SERVICES] isKindOfClass:[NSNull class]] ? dict[SERVICES] : [NSNull null]);
+        self.driveUpHrs = (![dict[DRIVE_UP_HRS] isKindOfClass:[NSNull class]] ? dict[DRIVE_UP_HRS] : [NSNull null]);
+        self.lobbyHrs = (![dict[LOBBY_HRS] isKindOfClass:[NSNull class]] ? dict[LOBBY_HRS] : [NSNull null]);
         
     }
     return self;
